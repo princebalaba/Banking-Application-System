69016468ad2f21b33d338e0d00e016267d6c8e50
JEB <JEB@JEBJOSH>
2022-03-04 17:35:34 -0500

    moved stuff around

--- Bank_Application/src/main/java/com/learning/dto/UserDto.java
+++ Bank_Application/src/main/java/com/learning/dto/UserDto.java
@@ -5,12 +5,15 @@ package com.learning.dto;
  * @time : 2022. 3. 4.-오후 4:23:39
  */
 public class UserDto {
+	
+
+	private Long id;
+	
 	private int password;
 
 	private String name;
 	
-	private Long id;
 	
-	private String address;
+	private String [] address;
 
 }
