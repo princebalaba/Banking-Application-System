11582326b4b6d15c4ef662085651dc884a245eac
Ki Beom Lee <klee16@ncsu.edu>
2022-03-04 17:39:38 -0500

    ,,

--- Bank_Application/src/main/java/com/learning/dto/UserDto.java
+++ Bank_Application/src/main/java/com/learning/dto/UserDto.java
@@ -5,12 +5,11 @@ package com.learning.dto;
  * @time : 2022. 3. 4.-오후 4:23:39
  */
 public class UserDto {
-	private int password;
+	
 
-	private String name;
 	private Long id;
 	
-	private String address;
-	
-	
+	private int password;
+
 }
+
