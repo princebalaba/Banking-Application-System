9de31e2ce531f59e0222c0689c60ed9cbaa182ef
JEB <JEB@JEBJOSH>
2022-03-04 17:38:19 -0500

    added date

--- Bank_Application/src/main/java/com/learning/dto/UserDto.java
+++ Bank_Application/src/main/java/com/learning/dto/UserDto.java
@@ -1,11 +1,14 @@
 package com.learning.dto;
 
+import java.time.LocalDate;
+
 /**
  * @author : Ki Beom Lee
  * @time : 2022. 3. 4.-오후 4:23:39
  */
 public class UserDto {
 	
+	private LocalDate date;
 
 	private Long id;
 	
