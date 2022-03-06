56c588e19d4ce40f8f7121b54c7ae47351761766
tacox <tacox@localhost>
2022-03-05 17:52:45 -0800

    Some classes

---
+++ Bank_Application/src/main/java/com/learning/entity/Role.java
@@ -0,0 +1,24 @@
+package com.learning.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table
+public class Role {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long roleId;
+	
+}
---
+++ Bank_Application/src/main/java/com/learning/entity/User.java
@@ -0,0 +1,32 @@
+package com.learning.entity;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+import javax.validation.constraints.NotBlank;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@MappedSuperclass
+
+public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	@NotBlank
+	private String userName;
+	@NotBlank
+	private String fullName;
+	@NotBlank
+	@JsonIgnore
+	private String password;
+
+}
