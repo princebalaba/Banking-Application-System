36420b83171596d6138ca505e81520c7da7683e0
princebalaba <kevin.balaba@blackburn.edu>
2022-03-07 17:47:25 -0600

    roles

--- Bank_Application/src/main/java/com/learning/entity/Role.java
+++ Bank_Application/src/main/java/com/learning/entity/Role.java
@@ -1,14 +1,19 @@
 package com.learning.entity;
 
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import com.learning.enums.ERole;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.NonNull;
 
 @Data
 @NoArgsConstructor
@@ -16,9 +21,11 @@ import lombok.NoArgsConstructor;
 @Entity
 @Table
 public class Role {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long roleId;
-	
+	@NonNull
+	@Enumerated(EnumType.STRING)
+	private ERole roleName;
 }
