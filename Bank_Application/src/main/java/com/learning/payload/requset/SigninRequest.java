f8db166dec6c913cdfab6539288a77a3e65fee8c
princebalaba <kevin.balaba@blackburn.edu>
2022-03-06 20:20:29 -0600

    payload signin

--- Bank_Application/src/main/java/com/learning/payload/requset/SigninRequest.java
+++ Bank_Application/src/main/java/com/learning/payload/requset/SigninRequest.java
@@ -3,7 +3,7 @@ package com.learning.payload.requset;
 import javax.validation.constraints.NotBlank;
 
 public class SigninRequest {
-	//sign in request info in payload
+	// sign in request info in payload
 	@NotBlank
 	private String userName;
 	@NotBlank
