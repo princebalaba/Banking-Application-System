86cde7e932e7ed64a18a46f9e015aac6ab4c8f33
tacox <tacox@localhost>
2022-03-08 13:40:22 -0800

    Repo with RoleRepo

---
+++ Bank_Application/src/main/java/com/learning/repo/RoleRepo.java
@@ -0,0 +1,13 @@
+package com.learning.repo;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.learning.entity.Role;
+import com.learning.enums.ERole;
+@Repository
+public interface RoleRepo extends JpaRepository<Role, Long> {
+	public Optional<Role> findByRoleName(ERole eRole);
+}
