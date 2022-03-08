15067d76cedf3e45193023be80afa6fba80b071a
Ki Beom Lee <klee16@ncsu.edu>
2022-03-08 15:56:20 -0500

    security

---
+++ Bank_Application/src/main/java/com/learning/security/WebSecurityConfig.java
@@ -0,0 +1,87 @@
+package com.learning.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.learning.advice.ControllerAdvice;
+import com.learning.security.jwt.AuthEntryPointJwt;
+import com.learning.security.jwt.AuthTokenFilter;
+import com.learning.security.security_service.UserDetailsServiceImpl;
+
+/**
+ * @author : Ki Beom Lee
+ * @time : 2022. 2. 23.-오후 4:41:40
+ */
+@Configuration
+@EnableWebSecurity // it will make sure that security env. is enabled
+@EnableGlobalMethodSecurity(prePostEnabled = true) //
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+	@Autowired
+	private UserDetailsServiceImpl userDetailsServiceImpl;
+
+	@Autowired
+	private AuthEntryPointJwt unauthorizedHandler;
+//	@Autowired
+//	private ControllerAdvice unauthorizedHandler ;
+//	@Scope("prototype") for more than one 
+	@Bean
+	public AuthTokenFilter authenticationJwtTokenFilter() {
+		return new AuthTokenFilter();
+	}
+
+	@Override // auth manager
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		// TODO Auto-generated method stub
+		auth.userDetailsService(userDetailsServiceImpl).passwordEncoder(passwordEncoder());
+	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+	@Override
+	public AuthenticationManager authenticationManagerBean() throws Exception {
+		// TODO Auto-generated method stub
+		return super.authenticationManagerBean();
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+
+		//core part of the security -> we can restrict the access of endpoints through this config.
+		//we can set unauthorized access through this. 
+		//we can provide direct go  access for sign up and sign in(authorizing hte res).
+		//apply token validation for end points.
+		//cors:
+		//
+		http.cors().and().csrf().disable().exceptionHandling() // end points 
+		.authenticationEntryPoint(unauthorizedHandler)
+		.and()
+		.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+		.and()
+		.authorizeRequests().antMatchers("/api/auth/**").permitAll()
+		.antMatchers("/api/food/**").authenticated()
+		.anyRequest().permitAll();
+
+		
+		
+		http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
+		
+		
+	
+	}
+
+}
