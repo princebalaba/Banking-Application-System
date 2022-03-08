3f72c2d9176f2237b29bb117dee06bd8e6364988
tacox <tacox@localhost>
2022-03-08 13:11:07 -0800

    Merge branch 'master' of https://github.com/KiLee16/bankApplication.git

--- Bank_Application/src/main/java/com/learning/entity/UserDto.java
+++ Bank_Application/src/main/java/com/learning/entity/UserDto.java
@@ -17,7 +17,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @MappedSuperclass
 
-public class UserDto {
+public class UserDTO {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
@@ -29,5 +29,4 @@ public class UserDto {
 	@JsonIgnore
 	private String password;
 
-}
 }
