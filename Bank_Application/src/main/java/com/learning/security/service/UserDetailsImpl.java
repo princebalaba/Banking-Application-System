1a591b9e1e5b6171201020a4b547fa3e866953b8
JEB <JEB@JEBJOSH>
2022-03-08 16:02:40 -0500

    pom xml jjwt

--- Bank_Application/src/main/java/com/learning/security/service/UserDetailsImpl.java
+++ Bank_Application/src/main/java/com/learning/security/service/UserDetailsImpl.java
@@ -1,4 +1,4 @@
-package com.learning.securityservice;
+package com.learning.security.service;
 
 import java.util.Collection;
 import java.util.List;
--- Bank_Application/src/main/java/com/learning/security/service/UserDetailsServiceImpl.java
+++ Bank_Application/src/main/java/com/learning/security/service/UserDetailsServiceImpl.java
@@ -1,4 +1,4 @@
-package com.learning.securityservice;
+package com.learning.security.service;
 
 import javax.transaction.Transactional;
 
