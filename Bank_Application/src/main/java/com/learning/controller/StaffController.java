4df3c4926a3b448bdc40dee7ef4abb4a48cb425f
JEB <JEB@JEBJOSH>
2022-03-09 15:00:35 -0500

    staff controller update

--- Bank_Application/src/main/java/com/learning/controller/StaffController.java
+++ Bank_Application/src/main/java/com/learning/controller/StaffController.java
@@ -1,5 +1,12 @@
 package com.learning.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/staff")
 public class StaffController {
+	
+	
 
 }
