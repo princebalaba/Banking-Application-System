cd2c4ebb1243d3c7a65f26a6d66ecdcfed52890b
princebalaba <kevin.balaba@blackburn.edu>
2022-03-09 14:19:24 -0600

    a few changes

---
+++ Bank_Application/src/main/java/com/learning/response/AccountApproaval.java
@@ -0,0 +1,20 @@
+package com.learning.response;
+
+import javax.validation.constraints.NotBlank;
+
+import com.learning.enums.EStatus;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AccountApproaval {
+	@NotBlank
+	private long accountNumber;
+	@NotBlank
+	private EStatus accountStatus;
+	
+
+}
