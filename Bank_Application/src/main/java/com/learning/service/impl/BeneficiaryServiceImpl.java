cb7dbb07d5d13dd1f2e774121bdbdc55bf463cb1
JEB <jebjosh@gmail.com>
2022-03-11 12:24:45 -0500

    beneficiary stuff

--- Bank_Application/src/main/java/com/learning/service/impl/BeneficiaryServiceImpl.java
+++ Bank_Application/src/main/java/com/learning/service/impl/BeneficiaryServiceImpl.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 import com.learning.entity.BeneficiaryDTO;
 import com.learning.enums.Approved;
 import com.learning.repo.BeneficiaryRepo;
+import com.learning.response.StaffApproveBeneficiaryResponse;
 import com.learning.service.BeneficiaryService;
 
 @Service
@@ -29,6 +30,12 @@ public class BeneficiaryServiceImpl  implements BeneficiaryService{
 //				
 //				
 //				;
+		return null;
+	}
+
+	@Override
+	public StaffApproveBeneficiaryResponse approveBeneficiary() {
+		// TODO Auto-generated method stub
 		return null;
 	}
 }
