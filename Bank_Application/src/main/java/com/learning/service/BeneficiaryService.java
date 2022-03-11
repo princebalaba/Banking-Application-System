6241e2bb1bca767058e88d34d428830f2df92ac6
JEB <jebjosh@gmail.com>
2022-03-11 12:24:23 -0500

    beneficiary stuff

--- Bank_Application/src/main/java/com/learning/service/BeneficiaryService.java
+++ Bank_Application/src/main/java/com/learning/service/BeneficiaryService.java
@@ -3,9 +3,11 @@ package com.learning.service;
 import java.util.List;
 
 import com.learning.entity.BeneficiaryDTO;
+import com.learning.response.StaffApproveBeneficiaryResponse;
 
 public interface BeneficiaryService {
 
 	
 	List<BeneficiaryDTO> getBeneficiaries();
+	StaffApproveBeneficiaryResponse approveBeneficiary();
 }
